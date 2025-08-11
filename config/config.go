ff10e5f15bb73dc9ea6a96286203d494ec4c5f51
chris <5303221@gmail.com>
2025-08-11 12:16:34 +0800

    feat: add --buildFlags

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 var (
-	Version       = "1.1.2"
+	Version       = "1.1.3"
 	WireCmd       = "github.com/google/wire/cmd/wire@latest"
 	NunuCmd       = "github.com/go-nunu/nunu@latest"
 	RepoBase      = "https://github.com/go-nunu/nunu-layout-base.git"
